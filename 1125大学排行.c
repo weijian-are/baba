447f36a5ff9371139296082207c43a3b05056c98
weijian-are <74294897+weijian-are@users.noreply.github.com>
2020-11-25 09:50:41 +0800

    Create 1125大学排行.c

---
+++ 1125大学排行.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main(){
+	int i;
+
+	printf("大学排行");
+	
+
+	scanf("%d",&i);
+
+	if(i==1){
+	
+		printf("清华大学");
+	}
+	else if(i==2){
+	
+		printf("北京大学");
+	}
+	else if(i==3){
+	
+		printf("中国科学技术大学");
+	}
+	else if(i==4){
+	
+		printf("浙江大学");
+	}
+	else if(i==5){
+	
+		printf("复旦大学");
+	}
+	else if(i==6){
+	
+		printf("南京大学");
+	}
+	else if(i==7){
+	
+		printf("上海交通大学");
+	}
+	else{
+	
+		printf("输入错误");
+	}
+}
