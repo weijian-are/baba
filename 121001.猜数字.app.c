564bb33d1f648867ace64cf9f21053f6a71ffa34
weijian-are <74294897+weijian-are@users.noreply.github.com>
2020-12-10 21:02:47 +0800

    Create 121001.猜数字.app.c

---
+++ 121001.猜数字.app.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+	int i=15,a;
+	
+	char a1[100];
+
+	char a2[]="小鸡炖蘑菇";
+	char a3[]="宝塔镇河妖";
+
+	printf("想玩游戏么，请先回答问题  天王盖地虎,__________!!\n");
+	scanf("%s",&a1);
+	if(strcmp(a1,a2)==0){
+	printf("你可以进入游戏\n");
+	while(1){
+		scanf("%d",&a);
+		if(a<i || a>i){
+		printf("你答错了");
+		}
+		else{
+		printf("恭喜你答对了");
+		}
+	}
+	}
+	else if(strcmp(a1,a3)==0){
+	printf("你不配进入游戏");
+	}
+	else{
+	
+		printf("无效输入");
+	}
+
+
+	
+}
